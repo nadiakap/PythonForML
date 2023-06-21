8446409cae796d8cd88db5870ace6b6eb506f651
nadiakap <nadiakap@optonline.net>
2023-06-21 09:57:50 -0400

    Update defaults_logreg.py

--- BankCustomerDefaults/defaults_logreg.py
+++ BankCustomerDefaults/defaults_logreg.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 import matplotlib.pyplot as plt 
+#rc - runtime configuration
 plt.rc('font', size=14)
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
